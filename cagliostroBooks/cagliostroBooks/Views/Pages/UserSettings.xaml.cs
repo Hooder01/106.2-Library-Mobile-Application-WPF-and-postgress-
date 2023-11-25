00a59451333cd64d16ef46d6a8ccdc15171fcfac
Hooder <hoodernew@gmail.com>
2023-11-25 18:47:10 +1300

    Beta : 0.9.0
    Added:
    
    1. Settings icon animation
    
    2. some UI elements for settings page
    
    Bugs Found:
    
    1. UI for settings icon working but image is not rotating like expected behind the code

--- cagliostroBooks/cagliostroBooks/Views/Pages/UserSettings.xaml.cs
+++ cagliostroBooks/cagliostroBooks/Views/Pages/UserSettings.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,5 +25,24 @@ namespace cagliostroBooks.Views.Pages
         {
             InitializeComponent();
         }
+
+
+        private void RotateImage()
+        {
+            RotateTransform rotateTransform = new RotateTransform();
+
+            RenderTransform = rotateTransform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0, To = 360, Duration = TimeSpan.FromSeconds(5), 
+
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+        // _Code behind the Rotating settings icon_ \\ (BUG!!!)
+
     }
 }
